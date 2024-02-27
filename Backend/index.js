0f9d73abf5ff02676c9c1a32f39e7ac728b6889e
csgaikwad <csgaikwad321@gmail.com>
2024-02-27 20:17:06 +0530

    Sever setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,32 @@
-import  express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import compression from 'compression';
+import bcrypt from 'bcrypt';
+import helmet from 'helmet';
+import jwt from 'jsonwebtoken';
+import passport from 'passport';
+import multer from 'multer';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
 
 const app= express();
 const port=8000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(compression());
+app.use(helmet());
+app.use(cookieParser());
+
+
+
+
+
 
 app.get("/",(req,res)=>{
     res.send("Hello");
