355ba78ea12e582d4fb45ccc57e7637bf7231c6d
csgaikwad <csgaikwad321@gmail.com>
2024-03-18 18:52:21 +0530

    skeleton added finished

--- Frontend/src/components/pages/Home.jsx
+++ Frontend/src/components/pages/Home.jsx
@@ -40,7 +40,7 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen h-auto py-4 xl:px-5 mb-20">
+    <div className="min-h-screen h-auto  xl:px-5 mb-20">
       {properties.length === 0 ? (
         <div className=" flex flex-col items-center justify-center lg:grid grid-cols-1 gap-7  lg:grid-cols-2 xl:grid-cols-3 sm:px-10 lg:place-content-center my-12">
           {Array.from({ length: 3 }).map((_, index) => (
