e09d82bbed583e828ff743a6ad191422a63040bc
csgaikwad <csgaikwad321@gmail.com>
2024-03-18 17:55:20 +0530

    skeleton added

--- Frontend/src/components/pages/Home.jsx
+++ Frontend/src/components/pages/Home.jsx
@@ -41,7 +41,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen h-auto py-4 xl:px-5 mb-20">
-      {!properties.length === 0 ? (
+      {properties.length != 0 ? (
         <div className="flex items-center justify-center min-h-screen">
           <Skeleton
             height={200}
