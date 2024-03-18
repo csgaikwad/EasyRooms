32ad42054fe13f006883aba32afb2063ccc70f0c
csgaikwad <csgaikwad321@gmail.com>
2024-03-18 19:33:58 +0530

    again minor fixes

--- Frontend/src/components/pages/Home.jsx
+++ Frontend/src/components/pages/Home.jsx
@@ -81,7 +81,7 @@ export default function Home() {
           ))}
         </div>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3   gap-4 m-8 justify-items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3   gap-4 m-4 sm:m-8 justify-items-center">
           {shuffledProperties.map((property) => (
             <div
               key={property._id}
