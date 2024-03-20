18e5727098fb5daa79f27276c1896da1d164c04b
csgaikwad <csgaikwad321@gmail.com>
2024-03-20 19:39:57 +0530

    mobile property option enabled

--- Frontend/src/components/pages/Home.jsx
+++ Frontend/src/components/pages/Home.jsx
@@ -85,7 +85,7 @@ export default function Home() {
           {shuffledProperties.map((property) => (
             <div
               key={property._id}
-              className="shadow-xl rounded-xl  md:min-w-[20rem] w-[100%] md:w-auto md:max-w-[30rem] bg-transparent hover:my-3 my-4 border-2 transition-transform duration-300 transform hover:scale-105 hover:z-0  hover:border-white hover:border-4 cursor-pointer"
+              className="shadow-xl rounded-xl  md:min-w-[20rem] w-[100%]  md:max-w-[30rem] bg-transparent hover:my-3 my-4 border-2 transition-transform duration-300 transform hover:scale-105 hover:z-0  hover:border-white hover:border-4 cursor-pointer"
             >
               <Carousel
                 showThumbs={false}
