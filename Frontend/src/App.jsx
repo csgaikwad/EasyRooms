61010400607948f0761ba0939a145d601ce45e73
csgaikwad <csgaikwad321@gmail.com>
2024-04-12 21:26:18 +0530

    exit animation added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -42,14 +42,14 @@ export default function App() {
     animate: {
       x: 0,
       transition: {
-        duration: 0.5,
+        duration: 0.4,
       },
     },
     exit: {
       // scale: 0.5,
       x: "-100vw",
       transition: {
-        duration: 0.5,
+        duration: 0.4,
       },
     },
   };
