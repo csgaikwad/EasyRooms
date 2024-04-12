73ad97f1e695e0778af1328cdd02e577bed78bae
csgaikwad <csgaikwad321@gmail.com>
2024-04-12 21:33:20 +0530

    exit animation added

--- Frontend/src/components/BookingWidget.jsx
+++ Frontend/src/components/BookingWidget.jsx
@@ -130,7 +130,8 @@ export default function BookingWidget(props) {
   return (
     <div className="flex flex-col justify-center items-center py-10">
       <h1 className=" text-xl lg:text-[2rem] font-sans font-semibold mb-4 ">
-        <div className="font-mono">
+          <span className="">10% off</span>
+        <div className="font-mono inline-block">
           <span className="line-through text-gray-800">
             ${parseInt(props.price)}
           </span>{" "}
