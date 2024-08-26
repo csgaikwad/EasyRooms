695ab77a04c0b5d6ecb57c2689fbb7f6018509e2
Shekhar <131723143+csgaikwad@users.noreply.github.com>
2024-08-26 22:46:35 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -17,8 +17,8 @@ import Terms from "./layout/Footer/Terms";
 import fetchData from "./utils/fetchData";
 import useScrollToTop from "./utils/scrollToTop";
 
-axios.defaults.baseURL = "https://easyrooms-ssg.koyeb.app";
-// axios.defaults.baseURL = "https://airbnd-qs5d.onrender.com";
+// axios.defaults.baseURL = "https://easyrooms-ssg.koyeb.app";
+axios.defaults.baseURL = "https://easyrooms.onrender.com";
 // axios.defaults.baseURL = "http://localhost:8000";
 axios.defaults.withCredentials = true;
 
